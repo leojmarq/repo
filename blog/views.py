cdb43b3b2f7e78bbd1955e33cb540d5aee1cdf3f
Leonardo Marquez <leojmarquez6@gmail.com>
2023-07-01 17:25:38 +0200

    post_update

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView
 from .models import Post
+from django.urls import reverse_lazy
+
 
 # Create your views here.
 
@@ -10,4 +13,15 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'port_detail_view.html'
+    template_name = 'post_detail_view.html'
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'Post_new.html'
+    fields = ['title', 'author', 'body']
+
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name= 'post_update.html'
+    fields = ['title','body']
+    
